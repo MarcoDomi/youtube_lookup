cbe7333dc3e93783a4bf40724eb417ab1d2a969e
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-25 15:57:51 -0500

    retrieve selected text when menu item is clicked

--- background.js
+++ background.js
@@ -1,7 +1,23 @@
 chrome.runtime.onInstalled.addListener(() => {
     chrome.contextMenus.create({
         title: "Youtube LookUp",
-        contexts: ["page"],
-        id: "page"
+        contexts: ["selection"],
+        id: "selection"
     });
+});
+
+function onCreated(tab) {
+  console.log(`Created new tab: ${tab.id}`);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    let selected_text = info.selectionText;
+    let new_tab = chrome.tabs.create({
+        url: `https://www.youtube.com/results?search_query=${selected_text}`,
+    });
+    new_tab.then(onCreated, onError);
 });
